604903fd212210889d8bc9ce46f70930baed142f
mahmoudashraf01 <dahmamahmoud87@gmail.com>
2024-06-20 20:37:22 +0300

    making navigation to home page by GETX

--- lib/features/spalsh/presentation/views/widgets/splash_view_body.dart
+++ lib/features/spalsh/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:books_app/core/utils/animated_text.dart';
 import 'package:books_app/core/utils/assets.dart';
+import 'package:books_app/features/spalsh/presentation/views/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -17,14 +19,8 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 3),
-    );
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 6), end: Offset.zero)
-            .animate(animationController);
-    animationController.forward();
+    initSlidingAnimation();
+    homeNavigation();
   }
 
   //dispose the controler to make sure there is no memory leak.
@@ -50,5 +46,26 @@ class _SplashViewBodyState extends State<SplashViewBody>
         ),
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 6), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  void homeNavigation() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(
+        () => const HomeView(),
+        transition: Transition.fade,
+        duration: const Duration(milliseconds: 250),
+      );
+    });
   }
 }
