cd0b5ad431ee9f088965a4f19e0208d829a22c35
mahmoudashraf01 <dahmamahmoud87@gmail.com>
2024-06-26 14:35:11 +0300

    implement feach newest books

--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -1,12 +1,27 @@
 import 'package:books_app/core/errors/failures.dart';
+import 'package:books_app/core/utils/api_service.dart';
 import 'package:books_app/features/home/data/Models/BookModel/book_model.dart';
 import 'package:books_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImol implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImol(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var endPoint =
+          'volumes?Filtering=free-ebooks&q=subject:programming&sorting=newest';
+      var data = await apiService.get(endPoint: endPoint);
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
